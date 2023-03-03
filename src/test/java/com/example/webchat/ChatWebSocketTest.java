adfd773800bda17f21b87633f594cffa46c3270f
Konor8812 <konor881@gmail.com>
2023-03-03 12:33:13 +0200

    CI attempt 16

---
+++ src/test/java/com/example/webchat/ChatWebSocketTest.java
@@ -0,0 +1,19 @@
+package com.example.webchat;
+
+
+import com.example.webchat.handler.ChatWebSocketHandler;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.socket.WebSocketSession;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class ChatWebSocketTest {
+
+    @Test
+    public void test(){
+        var handler = new ChatWebSocketHandler();
+        handler.afterConnectionEstablished(mock(WebSocketSession.class));
+        assertTrue(true);
+    }
+}
